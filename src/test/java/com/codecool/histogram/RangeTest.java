4e1a8dafc942d0b31bc8f29e7354eb81ff6e3909
Piotr Młudzik <piotr.mludzik@gmail.com>
2020-11-29 14:09:36 +0100

    polish the RangeTest code

--- src/test/java/com/codecool/histogram/RangeTest.java
+++ src/test/java/com/codecool/histogram/RangeTest.java
@@ -1,58 +1,52 @@
 package com.codecool.histogram;
 
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class RangeTest {
 
-    @Test
-    @Order(0)
-    public void range_fromSmallerThat0_illegalArgumentException () {
-        assertThrows(IllegalArgumentException.class, () -> new Range(-1, 1));
+    private final Range range = new Range(3, 7);
+
+    @Nested
+    public class RangeConstructor {
+        @Test
+        public void ValueIsSmallerThat0_returnIllegalArgumentException() {
+            assertThrows(IllegalArgumentException.class, () -> new Range(-1, 1));
+        }
+
+        @Test
+        public void ValuesAreInNormalRange_DoNotReturnAnyExceptions() {
+            assertDoesNotThrow(() -> range);
+        }
+    }
+
+    @Nested
+    public class IsInRange {
+        @Test
+        public void wordLengthIsInRange_returnTrue() {
+            assertTrue(range.isInRange("foobar"));
+        }
+
+        @Test
+        public void wordLengthEqualsToRangeFrom_returnTrue() {
+            assertTrue(range.isInRange("foo"));
+        }
+
+        @Test
+        public void wordLengthEqualsToRangeTo_returnTrue() {
+            assertTrue(range.isInRange("foobar1"));
+        }
+
+        @Test
+        public void wordLengthInOutOfRange_returnFalse() {
+            assertFalse(range.isInRange("foobarfoobar"));
+        }
     }
 
     @Test
-    @Order(1)
-    public void range_normalRange_doesNotAnyExeptions() {
-        assertDoesNotThrow(() -> new Range(3, 7));
-    }
-
-    @Test
-    @Order(2)
-    public void isInRange_wordLengthIsInRange_true() {
-        Range range = new Range(3, 7);
-        assertTrue(range.isInRange("foobar"));
-    }
-
-    @Test
-    @Order(3)
-    public void isInRange_wordLengthEqualsToRangeFrom_true() {
-        Range range = new Range(3, 7);
-        assertTrue(range.isInRange("foo"));
-    }
-
-    @Test
-    @Order(4)
-    public void isInRange_wordLengthEqualsToRangeTo_true() {
-        Range range = new Range(3, 7);
-        assertTrue(range.isInRange("foobar1"));
-    }
-
-    @Test
-    @Order(5)
-    public void isInRange_wordLengthInOutOfRange_false() {
-        Range range = new Range(3, 7);
-        assertFalse(range.isInRange("foobarfoobar"));
-    }
-
-    @Test
-    @Order(6)
-    public void isString_wordLengthInOutOfRange_false() {
-        Range range = new Range(3, 7);
-        assertEquals("3 - 7", range.toString());
+    public void isString_normalRange_returnExpectedString() {
+        assertEquals("3  - 7 ", range.toString());
     }
 }
