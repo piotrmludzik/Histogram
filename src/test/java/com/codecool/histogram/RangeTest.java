82ab79f60fbe2275b5760c7f24c64684ab2bd8a7
Piotr Młudzik <piotr.mludzik@gmail.com>
2020-12-01 21:32:04 +0100

    polish the RangeTest code after consultation

--- src/test/java/com/codecool/histogram/RangeTest.java
+++ src/test/java/com/codecool/histogram/RangeTest.java
@@ -2,51 +2,46 @@ package com.codecool.histogram;
 
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class RangeTest {
 
-    private final Range range = new Range(3, 7);
-
     @Nested
     public class RangeConstructor {
         @Test
-        public void ValueIsSmallerThat0_returnIllegalArgumentException() {
+        public void fromValueIsSmallerThat0_returnIllegalArgumentException() {
             assertThrows(IllegalArgumentException.class, () -> new Range(-1, 1));
         }
 
         @Test
-        public void ValuesAreInNormalRange_DoNotReturnAnyExceptions() {
-            assertDoesNotThrow(() -> range);
+        public void ToValuesIsSmallerThatFromValue_returnIllegalArgumentException() {
+            assertThrows(IllegalArgumentException.class, () -> new Range(7, 3));
         }
     }
 
     @Nested
     public class IsInRange {
-        @Test
-        public void wordLengthIsInRange_returnTrue() {
-            assertTrue(range.isInRange("foobar"));
-        }
+        private final Range range = new Range(3, 7);
 
-        @Test
-        public void wordLengthEqualsToRangeFrom_returnTrue() {
-            assertTrue(range.isInRange("foo"));
-        }
-
-        @Test
-        public void wordLengthEqualsToRangeTo_returnTrue() {
-            assertTrue(range.isInRange("foobar1"));
+        @ParameterizedTest(name = "word: {0}")
+        @ValueSource(strings = { "123", "1234", "1234567" })
+        public void wordLengthIsInRange_returnTrue(String word) {
+            assertTrue(range.isInRange(word));
         }
 
         @Test
         public void wordLengthInOutOfRange_returnFalse() {
-            assertFalse(range.isInRange("foobarfoobar"));
+            assertFalse(range.isInRange("12345678"));
         }
     }
 
     @Test
     public void isString_normalRange_returnExpectedString() {
+        Range range = new Range(3, 7);
+
         assertEquals("3  - 7 ", range.toString());
     }
 }
